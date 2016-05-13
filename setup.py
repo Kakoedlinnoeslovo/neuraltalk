a89a6d3aa702bf925746bcb628c2fd0eeb9dc353
thanabhat <thanabhat@gmail.com>
2016-05-13 20:08:47 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name='neuraltalk',
+      version='1.0',
+      description='NeuralTalk',
+      long_description=long_description,
+      license='BSD',
+      url='https://github.com/Thanabhat/neuraltalk',
+     )
